c27f4be128c86a802ce6544cef5d8538d08e44c3
gabidavila <gabidavila@gmail.com>
2017-07-19 17:23:33 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+self.internal_counter(@@genres)
+  end
+
+  def self.artist_count
+    self.internal_counter(@@artists)
+  end
+
+  def self.internal_counter(array)
+    all_items_counted = {}
+
+    array.each do |item|
+      all_items_counted[item] ||= 0
+      all_items_counted[item] += 1
+    end
+
+    all_items_counted
+  end
+end
